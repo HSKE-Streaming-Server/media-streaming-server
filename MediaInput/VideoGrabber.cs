64cb03fa1995801a4f6ea2dcbee3ff0c810eedf5
Kasleberkas <alexander-walk@aon.at>
2020-05-04 22:01:00 +0200

    Implemented VideoGrabber, added Config

--- MediaInput/VideoGrabber.cs
+++ MediaInput/VideoGrabber.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
+using System.Data;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace MediaInput
 {
@@ -9,10 +11,12 @@ namespace MediaInput
     {
         //Singleton
         private static VideoGrabber _singleton = null;
+        //DB Config
+        private IConfiguration Config { get; }
 
-        static VideoGrabber()
+        private VideoGrabber()
         {
-            _singleton = new VideoGrabber();
+            this.Config = new ConfigurationBuilder().AddJsonFile("VideoStreamDBConfig.json", false, true).Build();
         }
 
         public static VideoGrabber GetSingletonInstance()
@@ -22,28 +26,68 @@ namespace MediaInput
 
         public IEnumerable<string> GetAvailableCategories()
         {
-            //TODO: Categories? Sind hierbei die Anbieter (ZDF etc) gemeint, oder fällt dies für den Videograbber flach? (vgl. Sundtekgrabber - TV und Radio)
-            throw new NotImplementedException();
-        }
+            using (var dbConnection = new MySqlConnection(
+                $"Server={Config["MySqlServerAddress"]};" +
+                $"Database={Config["MySqlServerDatabase"]};" +
+                $"Uid={Config["MySqlServerUser"]};" +
+                $"Pwd={Config["MySqlServerPassword"]};")
+                )
+            {
+                var selectCommand = new MySqlCommand($"SELECT DISTINCT Category FROM content", dbConnection);
+                var adapter = new MySqlDataAdapter
+                {
+                    SelectCommand = selectCommand
+                };
 
-        public IEnumerable<IEnumerable<ContentInformation>> GetAvailableContentInformation()
+                var dataset = new DataSet();
+                adapter.Fill(dataset);
+                var rowCollection = dataset.Tables[0].Rows;
+                return (from DataRow entry in rowCollection
+                        select (string)entry[0]);
+            }
+        }
+        public IDictionary<string, IEnumerable<ContentInformation>> GetAvailableContentInformation()
         {
-            //TODO: Setup SqlConnection first
-            //Call GetAvailableContentInformation(string category) Foreach Category in GetAvailableCategories - probably bad performance (1 Iteration through Database foreach Category)
-            //Better: Create ContentInformation for every DatabaseRow and Add to spezific List - Problem: Category Count unknown?
-            throw new NotImplementedException();
+            return GetAvailableCategories().ToDictionary(category => category, GetAvailableContentInformation);
         }
 
         public IEnumerable<ContentInformation> GetAvailableContentInformation(string category)
         {
-            //Call GetAvailableContentInformation() and choose list where category == category of elements
-            throw new NotImplementedException();
+            using (var dbConnection = new MySqlConnection(
+                $"Server={Config["MySqlServerAddress"]};" +
+                $"Database={Config["MySqlServerDatabase"]};" +
+                $"Uid={Config["MySqlServerUser"]};" +
+                $"Pwd={Config["MySqlServerPassword"]};")
+                )
+            {
+                var selectCommand = new MySqlCommand($"SELECT * FROM content WHERE Category=@category", dbConnection);
+                selectCommand.Parameters.AddWithValue("@category", category);
+                var adapter = new MySqlDataAdapter
+                {
+                    SelectCommand = selectCommand
+                };
+
+                var dataset = new DataSet();
+                adapter.Fill(dataset);
+                var rowCollection = dataset.Tables[0].Rows;
+
+                return (from DataRow entry in rowCollection
+                        select new ContentInformation(
+                            (string)entry[0], (string)entry[1], (string)entry[2],
+                            Convert.ToBoolean(entry[3]), Convert.ToBoolean(entry[4]),
+                            new Uri((string)entry[5]), new Uri((string)entry[6]))).ToList();
+            }
         }
 
-        public Stream GetMediaStream(Guid contentId)
+        public Tuple<Uri, bool> GetMediaStream(string contentId)
         {
-            //TODO: Wait for Niklas Merge on Master - Updating ContentInformation and IGrabber
-            throw new NotImplementedException();
+            var content = GetAvailableContentInformation().Values.SelectMany(item => item);
+            var requestedContent = content.FirstOrDefault(item => item.Id == contentId);
+            if (requestedContent == null)
+                throw new Exception("Content with specified contentId does not exist in database");
+            return new Tuple<Uri, bool>(requestedContent.ContentLocation, requestedContent.TunerIsSource);
         }
+
+
     }
 }
