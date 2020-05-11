465732cc3a210f63e6b00970d3e2e67fa743add0
FelixB <felix.baiz@web.de>
2020-05-11 15:49:24 +0200

    change manager to singleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ServerManager>();
+            services.AddSingleton<ServerManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
