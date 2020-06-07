3046a2826e0cd2331acf5fdd1e4101eb5aded387
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-06-07 20:31:01 +0200

    add login.sql script to populate database with test accounts, fix issue with account class

--- API/Model/Account.cs
+++ API/Model/Account.cs
@@ -1,11 +1,14 @@
 
 
+using System.Text.Json.Serialization;
+
 namespace API.Model
 {
     public class Account{
 
+        [JsonPropertyName("username")]
         public string Username {get; set;}
-
+        [JsonPropertyName("password")]
         public string Password {get; set;}
 
     }
