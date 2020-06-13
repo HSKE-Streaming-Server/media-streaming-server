64765a7adadaf6360d079dd68901e0d6beba4c45
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-06-13 19:09:27 +0200

    expand script and add exclusions to gitignore

--- docker/web/get_latest_client_artifact.sh
+++ docker/web/get_latest_client_artifact.sh
@@ -1,6 +1,11 @@
-GITHUB_USER=""
-GITHUB_TOKEN=""
+GITHUB_USER="niklasstich"
+GITHUB_TOKEN="d47a66ffde5feeacebb860c933d78f327120de5e"
 
+sudo apt update
+sudo apt install -y curl jq unzip
 curl -sS -u "$GITHUB_USER":"$GITHUB_TOKEN" https://api.github.com/repos/hske-streaming-server/media-streaming-client/actions/artifacts > artifacts.json
 FAKEDL=$(jq '.artifacts | max_by(.id) | .archive_download_url' artifacts.json | tr -d '"')
 curl -sS -u "$GITHUB_USER":"$GITHUB_TOKEN" -L "$FAKEDL" -o dist.zip
+rm ./webroot -rf
+mkdir webroot
+unzip dist.zip -d webroot
