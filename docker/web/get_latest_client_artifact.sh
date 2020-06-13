646b70d7d93f15e53e3ed2f450540a730a6335a7
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-06-13 20:48:32 +0200

    change the script so it only considers artifacts from pushes to master

--- docker/web/get_latest_client_artifact.sh
+++ docker/web/get_latest_client_artifact.sh
@@ -1,10 +1,10 @@
 GITHUB_USER="niklasstich"
-GITHUB_TOKEN="d47a66ffde5feeacebb860c933d78f327120de5e"
+GITHUB_TOKEN="64f4ad0aeea3c719a1f280eb9a1d3dc0753a2502"
 
 sudo apt update
 sudo apt install -y curl jq unzip
 curl -sS -u "$GITHUB_USER":"$GITHUB_TOKEN" https://api.github.com/repos/hske-streaming-server/media-streaming-client/actions/artifacts > artifacts.json
-FAKEDL=$(jq '.artifacts | max_by(.id) | .archive_download_url' artifacts.json | tr -d '"')
+FAKEDL=$(jq '[.artifacts[] | select(.name=="dist_master")] | max_by(.id) | .archive_download_url' artifacts.json | tr -d '"')
 curl -sS -u "$GITHUB_USER":"$GITHUB_TOKEN" -L "$FAKEDL" -o dist.zip
 rm ./webroot -rf
 mkdir webroot
